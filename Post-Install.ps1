1637828cca604765a750c0b72dc29ca83efb37a8
fireph <fireph@users.noreply.github.com>
2018-10-28 21:20:33 -0700

    beginnings of post install script

---
+++ Post-Install.ps1
@@ -0,0 +1,80 @@
+#Requires -RunAsAdministrator
+
+$wc = New-Object System.Net.WebClient
+
+add-type @"
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+public class TrustAllCertsPolicy : ICertificatePolicy {
+    public bool CheckValidationResult(
+        ServicePoint srvPoint, X509Certificate certificate,
+        WebRequest request, int certificateProblem) {
+        return true;
+    }
+}
+"@
+$AllProtocols = [System.Net.SecurityProtocolType]'Ssl3,Tls,Tls11,Tls12'
+[System.Net.ServicePointManager]::SecurityProtocol = $AllProtocols
+[System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
+
+<#
+
+$powerPlan = Get-WmiObject -Namespace root\cimv2\power -Class Win32_PowerPlan -Filter "ElementName = 'High Performance'"
+$powerPlan = Get-CimInstance -Name root\cimv2\power -Class win32_PowerPlan -Filter "ElementName = 'High Performance'"
+$powerPlan.Activate()
+
+#>
+
+Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name LaunchTo -Value 1
+
+if (!(Get-Command choco -errorAction SilentlyContinue)) {
+    Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+} else {
+    choco upgrade chocolatey
+}
+#choco install -y googlechrome visualstudio2017community visualstudio2017-workload-nativedesktop geforce-experience steam
+
+$apps = [ordered]@{
+    "Battle.net"="https://us.battle.net/download/getInstaller?os=win&installer=Battle.net-Setup.exe";
+    "PS4 Remote Play"="https://remoteplay.dl.playstation.net/remoteplay/module/win/RemotePlayInstaller.exe"
+}
+
+Foreach ($h in $apps.GetEnumerator()) {
+    $confirmation = Read-Host "Would you like to install $($h.Name)? (y/n)"
+    if ($confirmation -ne 'y') {continue}
+    Write-Host "Installing" $h.Name
+    $outpath = "$PSScriptRoot/"+ $h.Name + ".exe"
+    $wc.DownloadFile($h.Value, $outpath)
+    Start-Process -Filepath $outpath -Wait
+    Remove-Item -Path $outpath
+}
+
+$confirmation = Read-Host "Would you like to install Qt? (y/n)"
+if ($confirmation -eq 'y') {
+    $qtTags = Invoke-WebRequest -Uri "https://api.github.com/repos/qt/qt5/tags" -UseBasicParsing | ConvertFrom-Json
+    $qtVersion = $null
+    Foreach ($tag in $qtTags)
+    {
+        if ($tag.name -eq $null) {continue}
+        if ($tag.name -match "-") {continue}
+        if (!$tag.name.StartsWith("v")) {continue}
+        $qtVersion = $tag.name
+        break
+    }
+    if ($qtVersion -ne $null) {
+        $majorVersion = $qtVersion -match "v\d+\.\d+"
+        if ($Matches.Length -ge 1) {
+            $majorVersion = $Matches[0].substring(1)
+            $totalVersion = $qtVersion.substring(1)
+            $qtUrl = "http://download.qt.io/official_releases/qt/" + $majorVersion + "/" + $totalVersion + "/qt-opensource-windows-x86-" + $totalVersion + ".exe"
+            $qtFilePath = "$PSScriptRoot/qt-opensource-windows-x86-" + $totalVersion + ".exe"
+            Write-Host "Downloading Qt from url:" $qtUrl
+            $wc.DownloadFile($qtUrl, $qtFilePath)
+            Start-Process -Filepath $qtFilePath -Wait
+        } else {
+            Write-Error "Qt version not in valid format"
+        }
+    } else {
+        Write-Error "No valid Qt version found"
+    }
+}
